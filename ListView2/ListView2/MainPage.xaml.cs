5f79d8a5f3ec266c2e69389db02bbed33fc79779
Jaime Radulovich <jradlov@gmail.com>
2018-05-31 20:53:53 -0700

    Add project files.

---
+++ ListView2/ListView2/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using ListView2.Models;
+
+
+namespace ListView2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            myListView.ItemsSource = new List<ContactGroup>
+            {
+                new ContactGroup("C", "C")
+                {
+                    new Contact {Name="Caroline", ImageUrl= "https://placeimg.com/100/100/people/5", Status="am i alive?"}
+                },
+                new ContactGroup("D", "D")
+                {
+                    new Contact {Name="Denise", ImageUrl= "https://placeimg.com/100/100/people/4", Status="resentful"}
+                },
+                new ContactGroup("J", "J")
+                {
+                    new Contact {Name="Jaime", ImageUrl= "https://placeimg.com/100/100/tech/any", Status="sure!"}
+                },
+                new ContactGroup("S", "S")
+                {
+                    new Contact {Name="Steph", ImageUrl= "https://placeimg.com/100/100/people/2", Status="not sure..."},
+                    new Contact {Name="Sophie", ImageUrl= "https://placeimg.com/100/100/people/3", Status="so happy"}
+                }
+            };      
+
+            /*
+            myListView.ItemsSource = new List<Contact>
+            {
+                //new Contact {Name="Jaime", ImageUrl= "http://lorempixel.com/100/100/people/1", Status="sure!"},
+                new Contact {Name="Jaime", ImageUrl= "https://placeimg.com/100/100/tech/any", Status="sure!"},
+                new Contact {Name="Steph", ImageUrl= "https://placeimg.com/100/100/people/2", Status="not sure..."},
+                new Contact {Name="Sophie", ImageUrl= "https://placeimg.com/100/100/people/3", Status="so happy"},
+                new Contact {Name="Denise", ImageUrl= "https://placeimg.com/100/100/people/4", Status="resentful"},
+                new Contact {Name="Caroline", ImageUrl= "https://placeimg.com/100/100/people/5", Status="am i alive?"}
+            }; */
+            
+        }
+	}
+}
---
+++ ListView2/ListView2/Models/ContactGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListView2.Models
+{
+    class ContactGroup : List<Contact>
+    {
+        public string Title { get; set; }
+        public string ShortTitle { get; set; }
+
+        public ContactGroup(string title, string shortTitle)
+        {
+            Title = title;
+            ShortTitle = shortTitle;
+        }
+
+    }
+}
+ 
